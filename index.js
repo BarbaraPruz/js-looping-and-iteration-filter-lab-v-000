559ba5ade3d7d62714b2a45cfeea660306f91859
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-27 23:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching (drivers, name){
-  return drivers.filter (function (driver) { driver === name; });
+  return drivers.filter (function (driver) { console.log(driver, name, (driver===name)); driver === name; });
 }
